33badcb33d0f9995e87ce1d677f64eb92f473601
Marcel Häßler <marcel.haessler@outlook.com>
2026-03-02 20:26:36 +0100

    first commit

---
+++ js/header.js
@@ -0,0 +1,15 @@
+document.querySelectorAll('h1').forEach(h1 => {
+    const text = h1.innerText;
+    h1.innerHTML = '';
+    [...text].forEach(char => {
+        if (char === ' ') {
+            h1.innerHTML += ' ';
+            return;
+        }
+        const span = document.createElement('span');
+        span.innerText = char;
+        const isUpper = char === char.toUpperCase() && char !== char.toLowerCase();
+        span.setAttribute('data-case', isUpper ? 'upper' : 'lower');
+        h1.appendChild(span);
+    });
+});
